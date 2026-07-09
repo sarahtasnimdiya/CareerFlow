663081b02758c6975af1056a934ed5ec8a7a1492
Sarah Tasnim Diya <sarahtasnim99@gmail.com>
2026-07-09 17:24:06 +0600

    database added

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT ||3000;
 const path = require('path');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/health', (req, res) => {
   res.status(200).json({ message: 'OK' });
+});
+
+app.get('/db-health', async (req, res) => {
+  const count = await prisma.user.count();
+  res.json({ message: `Database is healthy. User count: ${count}` });
+  
 });
 
 app.use((req, res) => {
