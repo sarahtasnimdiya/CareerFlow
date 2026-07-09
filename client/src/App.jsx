fedb590714a6b97f00dcd5c89e5375f1436c19e3
Sarah Tasnim Diya <sarahtasnim99@gmail.com>
2026-07-09 16:15:28 +0600

    frontend connected

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { Button } from "@heroui/react";
+import { useEffect, useState } from "react";
+
+
+function App() {
+  const [health, setHealth] = useState(null);
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API_URL + "/health")
+      .then((response) => response.json())
+      .then((data) => setHealth(data))
+      .catch((error) => console.error("Error fetching health data:", error));
+  }, []);
+
+  return (
+    <div className="p-10">
+      <Button color="primary">Check Backend Health</Button>
+      {health ? <p>backend says: {health.message}</p> : <p>loading...</p>}
+    </div>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT ||3000;
 
+app.use(cors());
+
 app.get('/health', (req, res) => {
-  res.status(200).send('OK');
+  res.status(200).json({ message: 'OK' });
 });
 
 app.listen(port, () => {
