aeb667075e19bdefafb7e89cfa32e410f6ea6a3b
Sarah Tasnim Diya <sarahtasnim99@gmail.com>
2026-07-09 16:45:30 +0600

    client path add

--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT ||3000;
+const path = require('path');
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/health', (req, res) => {
   res.status(200).json({ message: 'OK' });
+});
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.listen(port, () => {
