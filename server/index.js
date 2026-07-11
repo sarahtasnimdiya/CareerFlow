8db6ee9bb83172208e36a522f3f7f30f906a1e0d
Sarah Tasnim Diya <sarahtasnim99@gmail.com>
2026-07-11 22:21:09 +0600

    JWT added

--- server/index.js
+++ server/index.js
@@ -1,40 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = process.env.PORT ||3000;
-const path = require('path');
 
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const prisma = require('./lib/db');
 
-const bcrypt = require('bcryptjs');
+
+const authRoutes = require('./routes/auth');
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
 
-app.post('/register', async (req, res) => {
-  const { username, email, password } = req.body;
-  try {
-    const existingUser = await prisma.user.findUnique({ where: { email } });
-    if (existingUser) {
-      return res.status(409).json({ message: 'Email already exists' });
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await prisma.user.create({
-      data: {
-          username,
-          email,
-          password: hashedPassword,
-          role: 'CANDIDATE'
-        }
-      });
-      res.status(201).json({ message: 'User created successfully' });
-    } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error creating user' });
-  }
-});
+app.use('/api', authRoutes);
 
 
 app.get('/health', (req, res) => {
